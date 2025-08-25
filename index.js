3de0951860ea385279f6d81f0bad786093ed0729
FaridaBarkenkhoyeva <barkenkhoyeva.farida@outlook.com>
2025-08-25 20:59:38 +0200

    install sequelize

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require ("express")
+const express = require ("express");
+const sequelize  = require("./db/db");
+
 const app = express();
+
 
 app.get("/", (req, res) => {
     res.send("This is the homepage");
